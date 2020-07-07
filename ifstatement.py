58039961ec712269f207a2e1d732643575d3205b
= <mustafa.yucel0624@gmail.com>
2020-07-07 12:40:35 +0200

    i added my first doccumnet(if else  lopp)

---
+++ ifstatement.py
@@ -0,0 +1,9 @@
+password = "12345"
+name = "mustafa"
+
+isim = input("Please enter your name")
+
+if isim == name:
+    print(f"Hello {isim}! The password is :{password}")
+else:
+    print(f"Hallo {isim}! See you later")    
