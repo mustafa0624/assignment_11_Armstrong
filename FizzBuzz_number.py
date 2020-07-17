fb624797d94f330193782a24dc6102312330e253
= <mustafa.yucel0624@gmail.com>
2020-07-17 14:06:45 +0200

    FizzBuzz commited done

---
+++ FizzBuzz_number.py
@@ -0,0 +1,12 @@
+def FizzBuzz(a,b):
+    for i in range(a,b):
+        
+        if i % 3 == 0 and i % 5 != 0:
+            print("Fizz")
+        elif i % 5 == 0 and i % 3 != 0:
+            print("Buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
+FizzBuzz(1,100)      
