cccbad88745ca7b76ae7683f859c4433258a6ba7
= <mustafa.yucel0624@gmail.com>
2020-07-27 21:36:42 +0200

    sudoku assignment is done

---
+++ sudoku.py
@@ -0,0 +1,11 @@
+counter = 0
+print("---------------------")
+for i in (sudoku):
+    
+    
+    print(*i[:3],"|",*i[3:6],"|",*i[6:])
+    #counter = 0
+    counter += 1
+    if counter % 3 == 0 :
+        print("---------------------")
+    
